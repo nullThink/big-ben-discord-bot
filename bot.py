297dc073843a38338a1d071dcac9d4a8ef65aee0
nullThink <legoking43@gmail.com>
2021-06-19 01:02:18 -0500

    fixed get function and changed up hour denotations

--- bot.py
+++ bot.py
@@ -24,56 +24,75 @@ operatingHours = False
 async def h(ctx):
     await ctx.send("Command Prefix: .")
     await ctx.send("set_hours: Sets the hours between when the bot should run")
-    await ctx.send("     if 1 argument, resets to default if arg is r")
-    await ctx.send("else: .set_hours start_hour, am/pm, end_hour, am/pm")
+    await ctx.send(" .set_hours start_hour, am/pm, end_hour, am/pm")
     await ctx.send("get_hours: returns the hours of operation")
+    await ctx.send("reset: resets the hours of operation and makes run all day")
 
-#set hour intervals
+#gets the working hour range
 @client.command()
-async def get_hours(ctx):
-    await ctx.send(str(run_start_hour) + " - " + str(run_end_hour))
+async def hours(ctx):
+    global run_start_hour
+    global run_end_hour
+    global run_default
+    
+    if(not run_default):
+        await ctx.send(str(run_start_hour) + " - " + str(run_end_hour))
+    else:
+        await ctx.send("No declared hours.")
+
+@client.command()
+async def reset(ctx):
+    global run_start_hour
+    global run_end_hour
+    global run_default
+        
+    run_start_hour = 0
+    run_end_hour = 0
+    run_default = True
+    await ctx.send("Hours reset!")
 
 @client.command()
 async def set_hours(ctx, *args):
     
-    global run_default
     global run_start_hour
     global run_end_hour
+    global run_default
+            global overnight
     
-    if(len(args) == 1):
-        if(args[0] == "r"):
-            run_default = True
-            await ctx.send("Hours reset!")
-        else:
-            await ctx.send("Hours not reset!")
-            run_default = False
+    if(args[0] == "12" and args[1].lower() == "am"):
+        run_start_hour = 0
+    elif(args[0] == "12" and args[1].lower() == "pm"):
+        run_start_hour = 12
     else:
-        run_default = False;
-        
         run_start_hour = int(args[0])
+        
+        if(args[1].lower() == "pm"):
+            run_start_hour = run_start_hour + 12
+        
+    if(args[2] == "12" and args[3].lower() == "am"):
+        run_end_hour = 0
+    elif(args[2] == "12" and args[3].lower() == "pm"):
+        run_end_hour = 12
+    else:
         run_end_hour = int(args[2])
-    
-        if(args[1].upper() == "PM"):
-            run_start_hour += 12
-        
-        if(args[3].upper() == "PM"):
-            run_end_hour += 12
-        
-        await ctx.send("Hours of operation set to " + args[0] + " " + args[1].upper() + " - " + args[2] + " " + args[3].upper())
-        print(str(run_start_hour) + " - " + str(run_end_hour));
+            
+        if(args[3].lower() == "pm"):
+            run_end_hour = run_end_hour + 12
         
     if(run_end_hour < run_start_hour):
-        global overnight
         overnight = True
-        
-    return [run_start_hour, run_end_hour, run_default]
+    
+    run_default = False
+    
+    #print(str(run_start_hour) + " - " + str(run_end_hour))
 
 @client.event
 async def on_ready():
+        
     print("bot is ready")
-    await getChannels()
-    await checkForBong.start()
     
+    await getChannels()
+    await checkForBong.start()    
 
 @client.event
 async def getChannels():
@@ -82,7 +101,7 @@ async def getChannels():
                 if str(channel.type) == 'voice':
                     vc_list.append(channel)
 
-@client.command()
+@client.event
 async def joinAndBong(numBongs):
     
     for channels in vc_list:
@@ -108,32 +127,35 @@ async def joinAndBong(numBongs):
             print("Channel is empty");
     
 @client.event
-async def time_to_operate(time1, time2, default):
+async def time_to_operate(time1, time2):
     
     if(overnight):
-        if((time.localtime().tm_hour >= time1 and time.localtime().tm_hour <= time2 + 24) or default == True):
+        if((time.localtime().tm_hour >= time1 and time.localtime().tm_hour <= time2 + 24)):
             return True
         else:
             return False
     else:
-        if((time.localtime().tm_hour >= time1 and time.localtime().tm_hour <= time2) or default == True):
+        if((time.localtime().tm_hour >= time1 and time.localtime().tm_hour <= time2)):
             return True
         else:
             return False
 
 @tasks.loop(seconds=1)
 async def checkForBong():
-    operatingHours = await time_to_operate(run_start_hour, run_end_hour, run_default)
+    operatingHours = await time_to_operate(run_start_hour, run_end_hour)
     
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     
-    if(t.tm_min == 0 and t.tm_sec == 0):
-        numBongs = t.tm_hour % 12
+    if(t.tm_min == 0 and t.tm_sec == 0 and operatingHours):
+        
+        if(t.tm_hour == 12):
+            numBongs = 12
+        else:
+            numBongs = t.tm_hour % 12
         await joinAndBong(numBongs)
     # else:
     #     print("Not on the Hour!")
     
     
 client.run(TOKEN)
-bot.add_command(set_hours, h, get_hours)
